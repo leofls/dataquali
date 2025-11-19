086b66e414c94783602c9f87e897a531188d385a
Leandro F. <leo.f.l.s@hotmail.com>
2025-11-19 12:44:56 -0300

    update next.config.ts to remove localPatterns and add new image assets

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    unoptimized: true, // opcional se estiver testando localmente
     
-    localPatterns: [
-      { pathname: '/**' }
-    ]
+   
   },
 };
 
